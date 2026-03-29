951a773a1b26a5fd76e4a202f8f210c1f3ab8e26
yudaprama <yudhaprama@icloud.com>
2026-03-29 11:45:07 +0700

    chore: update dependencies and integrate Firebase
    - Added Firebase dependencies in pubspec.yaml and updated versions in pubspec.lock.
    - Configured Firebase initialization in main.dart with platform-specific options.
    - Updated Android and iOS project files to include GoogleService-Info.plist for Firebase integration.
    - Registered Firebase plugins in macOS and Windows generated files.
    
    This commit enhances the app's functionality by integrating Firebase services and ensuring all necessary configurations are in place.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <dart_duckdb/dart_duckdb_plugin_c_api.h>
+#include <firebase_core/firebase_core_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DartDuckdbPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DartDuckdbPluginCApi"));
+  FirebaseCorePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
 }
